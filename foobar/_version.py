31cc1d51241371e1b0f6ebf9f3d8f0e5fd920f40
jacksonllee <jacksonlunlee@gmail.com>
2019-08-07 19:11:21 -0500

    Auto-reformat with black/white

--- foobar/_version.py
+++ foobar/_version.py
@@ -1,2 +1,2 @@
 # This script is executed by setup.py.
-__version__ = '0.0.0'
+__version__ = "0.0.0"
--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import os
 from setuptools import find_packages, setup
 
 
-_PACKAGE_NAME = 'foobar'
+_PACKAGE_NAME = "foobar"
 _THIS_DIR = os.path.dirname(os.path.realpath(__file__))
 
-with open(os.path.join(_THIS_DIR, _PACKAGE_NAME, '_version.py')) as f:
+with open(os.path.join(_THIS_DIR, _PACKAGE_NAME, "_version.py")) as f:
     # get __version__
     exec(f.read())
 
-with open(os.path.join(_THIS_DIR, 'README.md')) as f:
+with open(os.path.join(_THIS_DIR, "README.md")) as f:
     _LONG_DESCRIPTION = f.read().strip()
 
 
@@ -17,24 +17,21 @@ def main():
     setup(
         name=_PACKAGE_NAME,
         version=__version__,  # noqa: F821
-        author='Jackson L. Lee',
+        author="Jackson L. Lee",
         packages=find_packages(),
         description=_PACKAGE_NAME,
         long_description=_LONG_DESCRIPTION,
-        license='(specify license)',
-
-        python_requires='>=3.6',
+        license="(specify license)",
+        python_requires=">=3.6",
         zip_safe=False,
-
         # these are minimal requirements
         # distinct from the exact version pins in requirements.txt
         install_requires=[],
-
         # list of strs which are the PyPI trove classifiers
         # see: https://pypi.org/classifiers/
         classifiers=[],
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
